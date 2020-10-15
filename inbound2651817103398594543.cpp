4365611a342acd3ff21cfdef90f6605e0c016cc0
RATUL1971 <72877124+RATUL1971@users.noreply.github.com>
2020-10-15 22:06:04 +0600

    Aponkar1971
    A*2

---
+++ inbound2651817103398594543.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    int result = 0;
+    printf("Enter the valu of A\n");
+    scanf("%d",&a);
+    result=a+2;
+    printf("The result is %d\n",result);
+    return 0;
+}
